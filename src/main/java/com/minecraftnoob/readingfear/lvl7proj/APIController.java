19360865c981b61b21af196217232c8496187190
ReadingFear <readingfear@gmail.com>
2022-09-22 07:10:13 -0700

    Small fixes

--- src/main/java/com/minecraftnoob/readingfear/lvl7proj/APIController.java
+++ src/main/java/com/minecraftnoob/readingfear/lvl7proj/APIController.java
@@ -1,11 +1,15 @@
 package com.minecraftnoob.readingfear.lvl7proj;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 @RestController
+@CrossOrigin
 public class APIController {
 	APIService Service;
 	APIController(APIService Service) {
@@ -15,6 +19,9 @@ public class APIController {
 	@ApiOperation(value = "Gets a player's profile", notes = "Query can be a username or UUID", response = String.class)
 	@ApiResponses(value = {@ApiResponse(code = 200, message = "Player found"), @ApiResponse(code = 404, message = "Player not found")})
 	String GetProfile(@RequestParam(value = "q") String Query) {
-		return Service.GetProfile(Query);
+		String Profile = Service.GetProfile(Query);
+		if (Profile.equals("Error"))
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Player not found");
+		return Profile;
 	}
 }
