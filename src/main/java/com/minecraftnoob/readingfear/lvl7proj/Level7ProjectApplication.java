a3c5943d4e8ba598c56dfa05d2732aa94e99beba
ReadingFear <readingfear@gmail.com>
2022-09-22 05:28:28 -0700

    Initial commit

---
+++ src/main/java/com/minecraftnoob/readingfear/lvl7proj/Level7ProjectApplication.java
@@ -0,0 +1,9 @@
+package com.minecraftnoob.readingfear.lvl7proj;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class Level7ProjectApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(Level7ProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/com/minecraftnoob/readingfear/lvl7proj/Level7ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minecraftnoob.readingfear.lvl7proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Level7ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
